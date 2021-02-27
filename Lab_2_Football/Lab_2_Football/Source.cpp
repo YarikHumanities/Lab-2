53e99b280c8de737fa9137b543b4037a16f12a3b
Yarik <yaroslav.matsapura@gmail.com>
2021-02-27 21:26:45 +0200

    First try

--- Lab_2_Football/Lab_2_Football/Source.cpp
+++ Lab_2_Football/Lab_2_Football/Source.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <filesystem>
+namespace fs = std::filesystem;
 int main() {
-
+	std::ofstream ofFile;
+	std::string path = "C:/Users/User/coding/Labs 1.2/Lab-2/Csv files";
+	for (const auto & entry : fs::directory_iterator(path))
+	{
+		//std::cout << entry.path() << std::endl;
+		ofFile.open(entry.path(), std::ofstream::app);
+		if (!ofFile.is_open()) {
+			std::cout << "Cannot open the file" << std::endl;
+		}
+		else {
+			std::cout << "File opened" << std::endl;
+		}
+		ofFile.close();
+	}
+	
 }
