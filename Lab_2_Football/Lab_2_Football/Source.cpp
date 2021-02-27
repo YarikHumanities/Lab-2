b8f7e9a89feae70fc2ac79d2b403503cfeefbea3
Yarik <yaroslav.matsapura@gmail.com>
2021-02-27 22:08:29 +0200

    Project core

--- Lab_2_Football/Lab_2_Football/Source.cpp
+++ Lab_2_Football/Lab_2_Football/Source.cpp
@@ -5,18 +5,30 @@
 namespace fs = std::filesystem;
 int main() {
 	std::ofstream ofFile;
+	std::ifstream inFile;
 	std::string path = "C:/Users/User/coding/Labs 1.2/Lab-2/Csv files";
 	for (const auto & entry : fs::directory_iterator(path))
 	{
 		//std::cout << entry.path() << std::endl;
-		ofFile.open(entry.path(), std::ofstream::app);
-		if (!ofFile.is_open()) {
+		inFile.open(entry.path(), std::ofstream::app);
+		if (!inFile.is_open()) {
 			std::cout << "Cannot open the file" << std::endl;
 		}
 		else {
 			std::cout << "File opened" << std::endl;
+			int quant = 0;
+			inFile >> quant;
+			std::string str;
+			inFile.ignore();
+			std::cout << quant << std::endl;
+			
+			while (!inFile.eof()) {
+				str = "";
+				getline(inFile, str);
+				std::cout << str << std::endl;
+			}
 		}
-		ofFile.close();
+		inFile.close();
 	}
 	
 }
