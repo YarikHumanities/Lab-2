54222ec09043fa81b6405238083b5df1a7af8d16
Sergey <sereganiceman@gmail.com>
2021-02-27 23:33:14 +0300

    Find scores without rows

--- Lab_2_Football/Lab_2_Football/Source.cpp
+++ Lab_2_Football/Lab_2_Football/Source.cpp
@@ -7,10 +7,18 @@ int main() {
 	std::ofstream ofFile;
 	std::ifstream inFile;
 	std::string path = "G:/lab rep/Lab-2/Csv files";
-	for (const auto & entry : fs::directory_iterator(path))
+	std::string path2 = "raiting.csv";
+	for (const auto& entry : fs::directory_iterator(path))
 	{
 		//std::cout << entry.path() << std::endl;
 		inFile.open(entry.path(), std::ofstream::app);
+		ofFile.open(path2, std::ofstream::app);
+		if (!ofFile.is_open()) {
+			std::cout << "Cannot open the second file" << std::endl;
+		}
+		else {
+			std::cout << "second file is opened\n";
+		}
 		if (!inFile.is_open()) {
 			std::cout << "Cannot open the file" << std::endl;
 		}
@@ -25,11 +33,28 @@ int main() {
 			while (!inFile.eof()) {
 				str = "";
 				getline(inFile, str);
+				//std::cout << str;
+				std::string name= std::string(str,0, str.find(','));
+				ofFile<<name<<": ";
+				int pos = str.find(',');
+				int score = 0;
+				for (int i = pos + 1; i < str.size(); i += 4)
+				{
+					if (str[i] >str[i + 2]) 
+					{
+						score += 3;
+					}
+					else if (str[i] == str[i + 2])
+					{
+						score += 1;
+					}
+				}
+				ofFile << score << "\n";
 				std::cout << str << std::endl;	
 			}
-			std::cout << "Push Test\n";
 		}
 		inFile.close();
+		ofFile.close();
 	}
 	
 }
